2b0b0736a535d37ece67f902fb7518c25e2d034f
Mathijs de Bruin <drbob@dokterbob.net>
2010-04-06 17:21:46 +0200

    Bug in queryset with published objects from manager.

--- metadata/managers.py
+++ metadata/managers.py
@@ -1,14 +1,22 @@
 from datetime import datetime
 
-from django.db.models import Manager
+from django.db.models import Manager, Q
 from django.contrib.sites.managers import CurrentSiteManager
 
 class PublicationManager(Manager):
     def get_query_set(self):
         qs = super(PublicationManager, self).get_query_set()
-        return qs.filter(publish=True,
-                         publish_date__lte=datetime.now(),   
-                         publish_time__lte=datetime.now())
+        qs = qs.filter(publish=True)
+        
+        # Either the date should be less than today or the date is today and the time should
+        # be less than now.
+        now = datetime.now()
+        current_date = datetime.date(now)
+        current_time = datetime.time(now)
+        qs = qs.filter(Q(publish_date__lte=current_date) | \
+                       (Q(publish_date__exact=current_date) & Q(publish_time__lte=current_time)))
+        
+        return qs
 
 class CurrentSitePublicationManager(CurrentSiteManager):
     def get_query_set(self):
