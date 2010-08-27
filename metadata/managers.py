7cbc721bcb973be8ec7c8ce7260b2600e77cf5dc
Mathijs de Bruin <drbob@dokterbob.net>
2010-08-27 14:55:04 +0200

    PEP8 and docstrings

--- metadata/managers.py
+++ metadata/managers.py
@@ -3,22 +3,33 @@ from datetime import datetime
 from django.db.models import Manager, Q
 from django.contrib.sites.managers import CurrentSiteManager
 
+
 class PublicationManager(Manager):
+    """ Manager yielding only currently published items. """
+
     def get_query_set(self):
         qs = super(PublicationManager, self).get_query_set()
         qs = qs.filter(publish=True)
-        
-        # Either the date should be less than today or the date is today and the time should
-        # be less than now.
+
+        # Either the date should be less than today or the date is today and
+        # the time should be less than now.
         now = datetime.now()
         current_date = datetime.date(now)
         current_time = datetime.time(now)
-        qs = qs.filter(Q(publish_date__lte=current_date) | \
-                       (Q(publish_date__exact=current_date) & Q(publish_time__lte=current_time)))
-        
+
+        qobject = Q(publish_date__lte=current_date) | \
+                   (Q(publish_date__exact=current_date) \
+                    & Q(publish_time__lte=current_time))
+
+        qs = qs.filter(qobject)
+
         return qs
 
+
 class CurrentSitePublicationManager(CurrentSiteManager):
+    """ Manager yielding only currently published items from the current
+        site. """     # BROKEN CODE WARNING. FIXME!
+
     def get_query_set(self):
-        qs = super(CurrentSitePublicationManager,  self).get_query_set()
-        return qs.filter(publish=True,publish_date__lte=datetime.now())
+        qs = super(CurrentSitePublicationManager, self).get_query_set()
+        return qs.filter(publish=True, publish_date__lte=datetime.now())
--- metadata/models.py
+++ metadata/models.py
@@ -10,125 +10,172 @@ from django.contrib.sites.models import Site
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.sites.managers import CurrentSiteManager
 
-
 from managers import *
+
 
 class DateAbstractBase(models.Model):
     """ Abstract base class with creation and modification date. """
-    
+
     class Meta:
         abstract = True
         ordering = ['-create_date', '-modify_date']
         get_latest_by = 'create_date'
-    
-    create_date = models.DateTimeField(auto_now_add=True, verbose_name=_('creation date'))
-    modify_date = models.DateTimeField(auto_now=True, verbose_name=_('modification date'))    
+
+    create_date = models.DateTimeField(auto_now_add=True,
+                                       verbose_name=_('creation date'))
+    modify_date = models.DateTimeField(auto_now=True,
+                                       verbose_name=_('modification date'))
+
 
 def default_publish_date():
     return datetime.now().date()
+
 
 def default_publish_time():
     now = datetime.now()
     return time(hour=now.hour, minute=now.minute, second=now.second)
 
+
 class PublicationAbstractBase(DateAbstractBase):
-    """ Abstract base class with publish option, creation, modification and publication date. """
+    """ Abstract base class with publish option, creation, modification and
+        publication date. """
 
     class Meta:
         abstract = True
-        ordering = ['-publish_date', '-publish_time'] + DateAbstractBase.Meta.ordering
+        ordering = ['-publish_date', '-publish_time'] + \
+                        DateAbstractBase.Meta.ordering
         get_latest_by = 'publish_date'
 
-    objects = models.Manager()        
+    objects = models.Manager()
     published = PublicationManager()
-    
-    publish_date = models.DateField(verbose_name=_('publication date'), default=default_publish_date, null=True, blank=True, db_index=True)
-    publish_time = models.TimeField(verbose_name=_('publication time'), default=default_publish_time, null=True, blank=True, db_index=True)
-    publish = models.BooleanField(verbose_name=_('published'), default=False, db_index=True)
-    
+
+    publish_date = models.DateField(verbose_name=_('publication date'),
+                                    default=default_publish_date,
+                                    null=True, blank=True, db_index=True)
+    publish_time = models.TimeField(verbose_name=_('publication time'),
+                                    default=default_publish_time,
+                                    null=True, blank=True, db_index=True)
+    publish = models.BooleanField(verbose_name=_('published'), default=False,
+                                  db_index=True)
+
     def get_next_by_published(self):
-        qs = self.__class__.published.filter(Q(publish_date__gt=self.publish_date) | \
-                (Q(publish_date=self.publish_date) & Q(publish_time__gt=self.publish_time)) )
-        
+        qobject = Q(publish_date__gt=self.publish_date) | \
+                   (Q(publish_date=self.publish_date) & \
+                    Q(publish_time__gt=self.publish_time))
+
+        qs = self.__class__.published.filter(qobject)
+
         if qs.count():
-            return qs.order_by('publish_date','publish_time')[0]
-        
-        return None
-        
-    def get_previous_by_published(self):
-        qs = self.__class__.published.filter(Q(publish_date__lt=self.publish_date) | \
-                (Q(publish_date=self.publish_date) & Q(publish_time__lt=self.publish_time)) )
-        
-        if qs.count():
-            return qs.order_by('-publish_date','-publish_time')[0]
+            return qs.order_by('publish_date', 'publish_time')[0]
 
         return None
+
+    def get_previous_by_published(self):
+        qobject = Q(publish_date__lt=self.publish_date) | \
+                   (Q(publish_date=self.publish_date) & \
+                    Q(publish_time__lt=self.publish_time))
+
+        qs = self.__class__.published.filter(qobject)
+
+        if qs.count():
+            return qs.order_by('-publish_date', '-publish_time')[0]
+
+        return None
+
 
 def get_default_sites():
     return [site.id for site in Site.objects.all()]
-    
+
+
 class SitesAbstractBase(models.Model):
     """ Abstract base class with sites selection and site manager. """
-    
+
     class Meta:
         abstract = True
-        
-    sites = models.ManyToManyField(Site, verbose_name=_('sites'), default=get_default_sites)
-    
+
+    sites = models.ManyToManyField(Site, verbose_name=_('sites'),
+                                   default=get_default_sites)
+
     objects = models.Manager()
     on_site = CurrentSiteManager(field_name='sites')
 
-class SitesPublicationAbstractBase(PublicationAbstractBase, SitesAbstractBase):
-    """ Abstract base class with sites selection and publication attributes. """
-    
+
+class SitesPublicationAbstractBase(PublicationAbstractBase,
+                                   SitesAbstractBase):
+    """ Abstract base class with sites selection and publication
+        attributes. """
+
     class Meta:
         abstract = True
 
-    objects = models.Manager()    
+    objects = models.Manager()
     published_on_site = CurrentSitePublicationManager(field_name='sites')
 
+
 class AuthorAbstractBase(models.Model):
+    """ Abstract base class adding an author field and a change_author
+        permission. """
+
     class Meta:
         abstract = True
-        permissions = (("change_author", ugettext("Change author")),)
-    
+        permissions = (("change_author", ugettext("Change author")), )
+
     def author_name(self):
         return self.author.get_full_name() or self.author
-    
+
     author = models.ForeignKey(User, verbose_name=_('author'))
 
+
 class DescriptionAbstractBase(models.Model):
+    """ Abstract base class adding a description field. """
+
     class Meta:
         abstract = True
-    
-    description = models.TextField(verbose_name=_('description'), null=True, blank=True)
+
+    description = models.TextField(verbose_name=_('description'),
+                                   null=True, blank=True)
+
 
 class TitleAbstractBase(models.Model):
+    """ Abstract base class adding a title field. """
+
     class Meta:
         abstract = True
-    
+
     def __unicode__(self):
         return self.title
-    
+
     title = models.CharField(max_length=255, verbose_name=_('title'))
 
+
 class SlugAbstractBase(models.Model):
+    """ Abstract base class adding a slug field. """
+
     class Meta:
         abstract = True
-    
-    slug = models.SlugField(max_length=50, verbose_name=_('slug'), db_index=True)
+
+    slug = models.SlugField(max_length=50, verbose_name=_('slug'),
+                            db_index=True)
+
 
 class CommentsAbstractBase(models.Model):
+    """ Abstract base class adding an `allow_comments` boolean field. """
+
     class Meta:
         abstract = True
 
-    allow_comments = models.BooleanField(default=True, verbose_name=_('allow comments'))
+    allow_comments = models.BooleanField(default=True,
+                                         verbose_name=_('allow comments'))
 
-class MetaDataAbstractBase(TitleAbstractBase, 
-                           SlugAbstractBase, 
-                           AuthorAbstractBase, 
-                           DescriptionAbstractBase, 
+
+class MetaDataAbstractBase(TitleAbstractBase,
+                           SlugAbstractBase,
+                           AuthorAbstractBase,
+                           DescriptionAbstractBase,
                            SitesPublicationAbstractBase,
                            CommentsAbstractBase):
+    """ Abstract base class containing all the above fields through
+        subclassing. """
+
     class Meta:
         abstract = True
